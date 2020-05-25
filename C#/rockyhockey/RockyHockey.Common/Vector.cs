10590452761ae5b920127df405596ff90fcdba73
Thomas Brücklmayr <thomas.bruecklmayr@stud.hs-kempten.de>
2020-05-25 17:38:59 +0200

    -changed: reworked Vector Class:     -added attributes for start coordinate, end coordinate, direction and length     -added function to calculate length     -added constructors to fill vector

--- C#/rockyhockey/RockyHockey.Common/Vector.cs
+++ C#/rockyhockey/RockyHockey.Common/Vector.cs
@@ -1,10 +1,57 @@
-﻿namespace RockyHockey.Common
+﻿using System;
+
+namespace RockyHockey.Common
 {
     /// <summary>
     /// Klasse, die einen 2D-Vektor repräsentiert
     /// </summary>
     public class Vector
     {
+        public Vector() { }
+        public Vector(Coordinate start, Coordinate end, Coordinate direction)
+        {
+            Start = start;
+            End = end;
+            VectorDirection = direction;
+
+            calcLength();
+        }
+
+        public Vector(Coordinate start, Coordinate end)
+        {
+            Start = start;
+            End = end;
+
+            calcLength();
+        }
+
+        public double calcLength()
+        {
+            if (VectorDirection == null)
+                calcDirection();
+
+            Position = new GameFieldPosition
+            {
+                X = Start.X,
+                Y = Start.Y
+            };
+            Direction = new GameFieldPosition
+            {
+                X = End.X,
+                Y = End.Y
+            };
+
+            return Length = Math.Sqrt(Math.Pow(VectorDirection.X, 2) + Math.Pow(VectorDirection.Y, 2));
+        }
+
+        private void calcDirection()
+        {
+            VectorDirection = new Coordinate
+            {
+                X = End.X - Start.X,
+                Y = End.Y - Start.Y
+            };
+        }
         /// <summary>
         /// X-Achse
         /// </summary>
@@ -14,5 +61,10 @@
         /// X-Length
         /// </summary>
         public GameFieldPosition Direction { get; set; }
+
+        public Coordinate Start { get; private set; }
+        public Coordinate End { get; private set; }
+        public Coordinate VectorDirection { get; private set; }
+        public double Length { get; private set; }
     }
 }
