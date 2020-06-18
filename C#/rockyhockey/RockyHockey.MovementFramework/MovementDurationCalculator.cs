eceb5e7780fd9ff655e4de32da1d5b07f6d46456
Thomas Brücklmayr <thomas.bruecklmayr@stud.hs-kempten.de>
2020-06-18 13:16:17 +0200

    -added: class to calculate duration of moving the bat from one position to another

---
+++ C#/rockyhockey/RockyHockey.MovementFramework/MovementDurationCalculator.cs
@@ -0,0 +1,41 @@
+﻿using RockyHockey.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockyHockey.MovementFramework
+{
+    static class MovementDurationCalculator
+    {
+        public static long getMovementDuration(Coordinate start, Coordinate end)
+        {
+            return getMovementDuration(new Vector(start, end));
+        }
+
+        public static long getMovementDuration(Vector movement)
+        {
+            double length = movement.Length * Config.Instance.SizeRatio;
+
+            return (long)(length / Config.Instance.MaxBatVelocity);
+        }
+
+        public static long getMovementDuration(IEnumerable<Coordinate> positions)
+        {
+            return getMovementDuration(positions.ToArray());
+        }
+
+        public static long getMovementDuration(Coordinate[] positions)
+        {
+            long duration = 0;
+
+            for (int a = 1; a < positions.Length; a++)
+            {
+                duration += getMovementDuration(positions[a - 1], positions[a]);
+            }
+
+            return duration;
+        }
+    }
+}
