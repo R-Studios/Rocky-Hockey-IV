41980d3ff86cd02aaef118fa00f6747c01173560
R-Studios <Roman.Wecker@gmx.de>
2020-07-05 14:43:16 +0200

    Abschnitte zur JavaGUI zum Abschlussbericht hinzugefügt

---
+++ C#/rockyhockey/RockyHockey.MotionCaptureFramework/MotionCaptureDumper.cs
@@ -0,0 +1,42 @@
+﻿using RockyHockey.Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockyHockey.MotionCaptureFramework
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Implementierung des MotionCaptureProvider
+    /// </summary>
+    public class MotionCaptureDumper
+    {
+        static Queue<string> strings = new Queue<string>();
+        static StreamWriter writer = new StreamWriter(File.OpenRead("positiondump.txt"), Encoding.UTF8);
+
+        public static void AddCapture(string str)
+        {
+            lock (strings)
+            {
+                strings.Enqueue(str);
+            }
+        }
+
+        public static void Run()
+        {
+            while (true)
+            {
+                lock (strings)
+                {
+                    while (strings.Count > 0)
+                    {
+                        writer.WriteLine(strings.Dequeue());
+                    }
+                }
+            }
+        }
+    }
+}
